2dec71f6f0c4d03bcb7f5017695793d8edb345d3
zubeyranwar <zubeyranwar@gmail.com>
2024-03-04 09:20:50 +0300

    finish footer section

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,6 +1,9 @@
 const Heading = () => {
   return (
-    <div>Heading</div>
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">Your Ideas, Documents, & Plans. Unified. Welcome to<span className="underline">PenPal</span></h1>
+
+    </div>
   )
 }
 
