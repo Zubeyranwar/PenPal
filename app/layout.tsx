821fa24aa980062a2966aa4fa98325a72b5e0d79
zubeyranwar <zubeyranwar@gmail.com>
2024-03-04 04:19:07 +0300

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
+import { cn } from '@/lib/utils'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+   subsets: ['latin'], 
+  })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("min-h-screen bg-background antialiased")}>{children}</body>
     </html>
   )
 }
