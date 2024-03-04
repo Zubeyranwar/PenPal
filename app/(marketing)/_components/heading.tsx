512d1af0c50087b700b43e86f0e78ee484050acc
zubeyranwar <zubeyranwar@gmail.com>
2024-03-04 09:25:26 +0300

    finish heading section

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button"
+
 const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
-      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">Your Ideas, Documents, & Plans. Unified. Welcome to<span className="underline">PenPal</span></h1>
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">PenPal</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Jotion is the connected workspace where <br />
+        better, faster work happens.
+      </h3>
+      <Button>Get PenPal</Button>
 
     </div>
   )
