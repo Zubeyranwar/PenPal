fe36b694b1696a01602c8872d459cee25261e3d8
zubeyranwar <zubeyranwar@gmail.com>
2024-03-05 05:28:01 +0300

    setup clerk authentication

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -6,12 +6,13 @@ import { SignInButton } from "@clerk/clerk-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+import Spinner from "@/components/spinner";
 
 const Heading = () => {
   const {isAuthenticated, isLoading} = useConvexAuth()
 
   return (
-    <div className="max-w-3xl space-y-4">
+    <div className="max-w-3xl space-y-4 mt-[150px]">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span>
       </h1>
@@ -21,8 +22,22 @@ const Heading = () => {
       </h3>
       {isLoading && (
         <div>
-          
+          <Spinner size="lg"/>
         </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button>
+          <Link href="/documents">Enter Jotion</Link>
+          <ArrowRight className="w-4 h-4 ml-2"/>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button> 
+            Get Jotion free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
       )}
     </div>
   )
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,7 +1,51 @@
-function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
-}
+"use client"
+import { useScrollTop } from "@/hooks/useScrollTop";
+import { cn } from "@/lib/utils";
+import { useConvexAuth } from "convex/react";
+import Logo from "./logo";
+import Spinner from "@/components/spinner";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-export default Navbar
+const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  const scrolled = useScrollTop();
+  return (
+    <div
+      className={cn(
+        "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
+      <Logo />
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Get PenPal free
+              </Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm">
+              <Link href="/documents">Enter PenPal</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/"/>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
